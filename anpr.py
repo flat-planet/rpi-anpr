f6eca68af5408e5d1c582dab729035b9f01e0934
Colin MacGiollaEain <colin@flat-planet.net>
2020-05-11 21:53:14 +0100

    Improved comments

--- anpr.py
+++ anpr.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+# Hackey but needed to stop the underlying dependenices from bombing out
 import locale
 locale.setlocale(locale.LC_ALL, 'C')
 from openalpr import Alpr
@@ -37,7 +38,9 @@ def main():
     mainLogger.info('Capturing initial image for calibration')
     camera.capture('/home/pi/ScriptStartup.jpg')
 
+    # Store the latest snapshot in a tmpfs / RAM FS to save wear on the SDCARD
     snapshot = '/run/ANPR/latest.jpg'
+    # Eventually move this to a more permenent location, but again saving wear on the SDCARD
     dataStore = '/run/ANPR/'
     # Declaring this outside of the loop initially
     captureTS = time.strftime("%Y%m%d-%H%M%S")
@@ -45,17 +48,27 @@ def main():
 
     try:
         while True:
+            # Refrest the timestamp
             captureTS = time.strftime("%Y%m%d-%H%M%S")
+            # Take a snap
             camera.capture(snapshot)
             mainLogger.debug('Image captured at %s' % captureTS)
 
             # Lets see what the analysis thinks
             analysis = alpr.recognize_file(snapshot)
             if len(analysis['results']) == 0:
+                # We didn't recognise any licence plates in the current snapshit
                 mainLogger.debug('No licence plates recognised')
             else:
+                # Maybe we got something
                 mainLogger.info('Plate: %s detected with confidence of: %s' % ( analysis['results'][0]['plate'], analysis['results'][0]['confidence'] ) )
                 fName = dataStore + '/IMG_' + captureTS
+
+                # Since I don't trust this library to reliable detect licence plates yet, I'm using this as a chance to capture potential matches, as
+                # well as the analysis, to improve the training of the model
+                # So I save the jpg and the original analysis results
+                #
+                # Once it is reliable (>0.95), the goal would be to have it feed this information to other platforms to allow for analysis and alerting
                 mainLogger.debug('Moving %s to %s' % (snapshot, fName) )
                 shutil.move(snapshot, fName + '.jpg')
                 with open(fName+'.json') as results: 
@@ -65,6 +78,7 @@ def main():
             
             # Pause loop for 5 seconds
             time.sleep(5)
+
     except KeyboardInterrupt:
         mainLogger.info('Shutting down')
         alpr.unload()
